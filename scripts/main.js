777de2c80ac902a68d29833ba7248cec9f5b1867
Isaac Lee <lee3650@purdue.edu>
2022-04-06 22:18:00 -0400

    Converted button to link

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,21 @@
 const body = document.querySelector("body");
+let bg = 0; 
+let classes = ["light", "dark", "code"];
 
 function toggleMode() {
   const toggle = document.getElementById("toggle"); 
-  var text = toggle.textContent; 
-  console.log("Clicked with contents " + toggle.textContent); 
-  if (text == "Dark Mode") {
-    body.className = "dark"; 
-    toggle.textContent = "Light Mode"; 
-  }
-  else {
-    body.className = "light"; 
-    toggle.textContent = "Dark Mode"; 
-  }
+  bg = (bg + 1) % 3; 
+  body.className = classes[bg];
+  toggle.textContent = "Background"; 
+  //var text = toggle.textContent; 
+  //console.log("Clicked with contents " + toggle.textContent); 
+  //if (text == "Dark Mode") {
+  //body.className = "dark"; 
+  //toggle.textContent = "Light Mode"; 
+  //}
+  //else {
+  //body.className = "light"; 
+  //toggle.textContent = "Dark Mode"; 
+  //}
+  //else 
 }
