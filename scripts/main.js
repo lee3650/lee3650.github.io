7a038d41bb0cd443969aa64b9872b705bf5f3050
Isaac Lee <lee3650@purdue.edu>
2022-04-07 16:15:19 -0400

    Added 'about me' section

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,24 @@
 const body = document.querySelector("body");
 let bg = 0; 
 let classes = ["light", "dark", "code"];
+const content = document.getElementById("content"); 
+const welcome = document.getElementById("welcome-text"); 
+
+function aboutClicked() { 
+  content.className = "visible"; 
+  welcome.className = "hidden"; 
+}
+
+function showWelcome() {
+  welcome.className = "welcome-shown"; 
+  const tohide = document.getElementsByClassName("visible"); 
+  for (let i = tohide.length - 1; i >= 0; i--) {
+    tohide[i].className = "hidden"; 
+  }
+}
 
 function toggleMode() {
   const toggle = document.getElementById("toggle"); 
-  bg = (bg + 1) % 3; 
+  bg = (bg + 1) % classes.length; 
   body.className = classes[bg];
-  toggle.textContent = "Background"; 
-  //var text = toggle.textContent; 
-  //console.log("Clicked with contents " + toggle.textContent); 
-  //if (text == "Dark Mode") {
-  //body.className = "dark"; 
-  //toggle.textContent = "Light Mode"; 
-  //}
-  //else {
-  //body.className = "light"; 
-  //toggle.textContent = "Dark Mode"; 
-  //}
-  //else 
 }
