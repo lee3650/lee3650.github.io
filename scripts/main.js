483bfe9525cdcd9d80ed9f518c58887a0a2f990a
Isaac Lee <lee3650@purdue.edu>
2022-04-07 22:27:15 -0400

    Added some project descriptions

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,49 @@
 const body = document.querySelector("body");
 let bg = 0; 
-let classes = ["light", "dark", "code"];
-const content = document.getElementById("content"); 
+let classes = ["code", "dark", "light"];
+const content = document.getElementById("about"); 
 const welcome = document.getElementById("welcome-text"); 
 
 function aboutClicked() { 
-  content.className = "visible"; 
+  hideAll(); 
+  makeVisible(content); 
+}
+
+function specificProject(sender) {
+  console.log("clicked button with text " + sender.innerText); 
+  const e = document.getElementById(sender.innerText); 
+  hideAll(); 
+  makeVisible(e); 
+}
+
+function makeHidden(item) {
+  item.classList.remove("visible"); 
+  item.classList.add("hidden"); 
+}
+
+function makeVisible(item) {
+  item.classList.remove("hidden"); 
+  item.classList.add("visible"); 
+}
+
+function hideAll() {
+  const tohide = document.getElementsByClassName("visible"); 
+  for (let i = tohide.length - 1; i >= 0; i--) {
+    let item = tohide[i];
+    makeHidden(item); 
+  }
   welcome.className = "hidden"; 
 }
 
+function projectsClicked() {
+  const projects = document.getElementById("project-intro"); 
+  hideAll();
+  makeVisible(projects); 
+}
+
 function showWelcome() {
+  hideAll();
   welcome.className = "welcome-shown"; 
-  const tohide = document.getElementsByClassName("visible"); 
-  for (let i = tohide.length - 1; i >= 0; i--) {
-    tohide[i].className = "hidden"; 
-  }
 }
 
 function toggleMode() {
